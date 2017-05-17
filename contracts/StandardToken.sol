297df44e588f782b534d2e6db2fe208e279eb4c3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2017-05-17 13:09:18 +0300

    Backported approve() race condition mitigation.

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -30,10 +30,11 @@ contract StandardToken is ERC20, SafeMath {
      _;
   }
 
-  function transfer(address _to, uint _value) onlyPayloadSize(2 * 32) returns (bool success) {
+  function transfer(address _to, uint _value) returns (bool success) {
     balances[msg.sender] = safeSub(balances[msg.sender], _value);
     balances[_to] = safeAdd(balances[_to], _value);
     Transfer(msg.sender, _to, _value);
+    return true;
   }
 
   function transferFrom(address _from, address _to, uint _value) returns (bool success) {
@@ -46,6 +47,7 @@ contract StandardToken is ERC20, SafeMath {
     balances[_from] = safeSub(balances[_from], _value);
     allowed[_from][msg.sender] = safeSub(_allowance, _value);
     Transfer(_from, _to, _value);
+    return true;
   }
 
   function balanceOf(address _owner) constant returns (uint balance) {
@@ -53,6 +55,13 @@ contract StandardToken is ERC20, SafeMath {
   }
 
   function approve(address _spender, uint _value) returns (bool success) {
+
+    // To change the approve amount you first have to reduce the addresses`
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  already 0 to mitigate the race condition described here:
+    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
+
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
