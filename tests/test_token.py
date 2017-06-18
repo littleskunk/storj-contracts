74dd89e7a0cd794ae80d94f5996cbbb42e70fd96
Mikko Ohtamaa <mikko@opensourcehacker.com>
2017-06-17 17:02:53 -0700

    Removed extra tests.

--- tests/test_token.py
+++ tests/test_token.py
@@ -95,24 +95,3 @@ def test_transfer_with_allowance_exceeded(token_with_customer_balance: Contract,
         token.transact({"from": allowed_party}).transferFrom(customer, empty_address, amount+1)
 
 
-
-def test_increase_approval(token: Contract, customer: str, empty_address: str, allowed_party):
-    """Increase approval."""
-
-    amount = 5000
-    change = 1000
-    assert token.call().allowance(customer, allowed_party) == 0
-    token.transact({"from": customer}).approve(allowed_party, amount)
-    token.transact({"from": customer}).addApproval(allowed_party, change)
-    assert token.call().allowance(customer, allowed_party) == amount + change
-
-
-def test_decrease_approval(token: Contract, customer: str, empty_address: str, allowed_party):
-    """Decrease approval."""
-
-    amount = 5000
-    change = 1000
-    assert token.call().allowance(customer, allowed_party) == 0
-    token.transact({"from": customer}).approve(allowed_party, amount)
-    token.transact({"from": customer}).subApproval(allowed_party, change)
-    assert token.call().allowance(customer, allowed_party) == amount - change
