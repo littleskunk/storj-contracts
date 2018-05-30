849ce0ebd94156fc65795d1f7e010ff592a697c0
littleskunk <jens.heimbuerge@googlemail.com>
2018-05-30 23:42:11 +0200

    Multisend contract for cheaper and faster payouts

---
+++ contracts/Multisend.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    owner = newOwner;
+  }
+
+}
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20 {
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract Multisend is Ownable {
+
+  function multisend(address _tokenAddr, address[] _to, uint256[] _value) onlyOwner public returns (bool _success) {
+    require(_to.length == _value.length);
+    // loop through to addresses and send value
+    for (uint8 i = 0; i < _to.length; i++) {
+      ERC20(_tokenAddr).transfer(_to[i], _value[i]);
+    }
+    return true;
+  }
+}
